2e523813dce24d517f02f3cf32375da37a9b1793
galaxysha512 <galaxysha512@gmail.com>
2018-09-21 21:12:55 +0800

    fix

--- src/main/java/com/sendtomoon/MyBatisSpring/common/CommonBatis.java
+++ src/main/java/com/sendtomoon/MyBatisSpring/common/CommonBatis.java
@@ -1,15 +1,17 @@
 package com.sendtomoon.MyBatisSpring.common;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
-@Component
+@Repository
 public @interface CommonBatis {
-
+	String value() default "";
 }
