d3a9ab683d79cfa16590784cd25893c3f6269932
galaxysha512 <galaxysha512@gmail.com>
2018-09-24 00:32:45 +0800

    commit

---
+++ src/main/java/com/sendtomoon/MyBatisSpring/service/TestSerivce.java
@@ -0,0 +1,7 @@
+package com.sendtomoon.MyBatisSpring.service;
+
+import com.sendtomoon.MyBatisSpring.common.Error1Exception;
+
+public interface TestSerivce {
+	public String test1() throws Error1Exception;
+}
