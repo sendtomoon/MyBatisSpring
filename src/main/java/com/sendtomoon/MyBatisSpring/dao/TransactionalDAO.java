8eec8db39e7d16706803fc30f9b3f569b14be251
galaxysha512 <galaxysha512@gmail.com>
2018-09-26 10:54:55 +0800

    fix

--- src/main/java/com/sendtomoon/MyBatisSpring/dao/TransactionalDAO.java
+++ src/main/java/com/sendtomoon/MyBatisSpring/dao/TransactionalDAO.java
@@ -1,7 +1,8 @@
 package com.sendtomoon.MyBatisSpring.dao;
 
+import com.sendtomoon.MyBatisSpring.common.CommonBatis;
 import com.sendtomoon.MyBatisSpring.entity.TestDTO;
-
+@CommonBatis
 public interface TransactionalDAO {
 
 	void insert(TestDTO name);
